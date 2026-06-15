b8fa412dd3985a9e1a08c67ff06f1fd6e58209cb
Eklavya Sharma <ekurgn@gmail.com>
2026-06-15 18:59:15 -0500

    OddTimesOddIsOdd: remove Mathlib

--- SimpleExamples/OddTimesOddIsOdd.lean
+++ SimpleExamples/OddTimesOddIsOdd.lean
@@ -1,11 +1,14 @@
-import Mathlib.Algebra.Ring.Parity  -- for Odd
-import Mathlib.Tactic.Ring  -- for the ring tactic
+def Odd (n: Int) : Prop := ∃ k, n = 2 * k + 1
 
-lemma simplify_product (a b : Int)
+theorem simplify_product (a b : Int)
     : (2 * a + 1) * (2 * b + 1)
-      = 2 * (2 * a * b + a + b) + 1
-    := by
-  ring
+      = 2 * (a * (2 * b + 1) + b) + 1
+    := calc
+  (2 * a + 1) * (2 * b + 1)
+      = (2 * a) * (2 * b + 1) + 1 * (2 * b + 1)  := by rw [Int.add_mul]
+    _ = 2 * (a * (2 * b + 1)) + (2 * b + 1)  := by rw [Int.mul_assoc, Int.one_mul]
+    _ = (2 * (a * (2 * b + 1)) + 2 * b) + 1  := by rw [Int.add_assoc]
+    _ = 2 * (a * (2 * b + 1) + b) + 1  := by rw [←Int.mul_add]
 
 theorem odd_times_odd_is_odd (m n : Int)
     (hM : Odd m) (hN : Odd n)
@@ -16,4 +19,4 @@ theorem odd_times_odd_is_odd (m n : Int)
   unfold Odd at hN
   obtain ⟨b, hB⟩ := hN
   rw [hB]
-  exact ⟨2 * a * b + a + b, simplify_product a b⟩
+  exact ⟨a * (2 * b + 1) + b, simplify_product a b⟩
