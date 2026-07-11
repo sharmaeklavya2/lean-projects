bf04f33f9a6d094e6c90df6b4b1aa11375ce715f
Eklavya Sharma <ekurgn@gmail.com>
2026-07-10 22:27:07 -0500

    BinPack.Harmonic: genericize definitions

--- BinPack/Harmonic/Core.lean
+++ BinPack/Harmonic/Core.lean
@@ -6,15 +6,17 @@ public import Mathlib.Algebra.Order.Floor.Ring
 
 @[expose] public section
 
-noncomputable def rawcat (x : ℝ) : ℕ :=
+variable {α : Type*} [Field α] [LinearOrder α] [IsStrictOrderedRing α] [FloorRing α]
+
+def rawcat (x : α) : ℕ :=
   (⌊1/x⌋.toNat)
 
-noncomputable def cat (M : ℕ) (x : ℝ) : ℕ :=
+def cat (M : ℕ) (x : α) : ℕ :=
   -- M ≥ 1 and x ∈ (0, 1]
   -- category of an item of size x
   min M (⌊1/x⌋.toNat)
 
-noncomputable def wh (M : ℕ) (x : ℝ) : ℝ :=
+def wh (M : ℕ) (x : α) : α :=
   -- M ≥ 1 and x ∈ (0, 1]
   -- harmonic weight of an item of size x
   if x ≤ 1/M then M * x / (M-1) else 1 / (⌊1/x⌋.toNat)
